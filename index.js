a08503c524c7630e0f232d0146034a7828152a5e
andyotarola <otarolaandy@gmail.com>
2024-12-11 17:29:43 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import admin from 'firebase-admin';
+
+admin.initializeApp({
+    credential: admin.credential.cert('./serviceAccountKey.json'),
+    databaseURL: "https://miboticadev-1586837070418.firebaseio.com"
+});
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.post('/subscribe-subsidiarie', (req, res) => {
+    const { token } = req.body;
+
+    admin.messaging().subscribeToTopic([token], '1540-3221')
+        .then(response => {
+            console.log(response);
+            res.send('Subscribed to topic');
+        })
+        .catch(error => {
+            console.log(error);
+            res.send('Error subscribing to topic');
+        });
+
+});
+
+app.post('/subscribe-company', (req, res) => {
+    const { token } = req.body;
+
+    admin.messaging().subscribeToTopic([token], '314-3221')
+        .then(response => {
+            console.log(response);
+            res.send('Subscribed to topic');
+        })
+        .catch(error => {
+            console.log(error);
+            res.send('Error subscribing to topic');
+        });
+
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
